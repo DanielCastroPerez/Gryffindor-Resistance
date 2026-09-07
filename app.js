4b031cbfff4b3c53353cbab07be3a1d5a35be284
Diego González Celis <diegoglez16082001@hotmail.com>
2026-09-07 14:53:51 -0600

    evento submit añadido

--- app.js
+++ app.js
@@ -205,3 +205,42 @@ const renderCard = (albumObject, htmlElement) => {
     htmlElement.insertAdjacentHTML("beforeend", card);
 }
 
+
+
+
+
+
+const formVol = document.getElementById("recruitment-form");
+console.log(formVol);
+
+const mainVol = document.querySelector("#por-definir-xd")
+console.log(mainVol);
+
+let voluntarios = [];
+
+formVol.addEventListener("submit", (event) => {
+    event.preventDefault();
+    //console.log(event);
+    const formData = new FormData(formVol);
+    console.log(formData);
+    const dataArray = [...formData];
+    console.log(dataArray);
+
+    //console.log(dataArray[2]);
+    const voluntario = Object.fromEntries(dataArray);
+    console.log(voluntario);
+
+    voluntarios.push(voluntario);
+
+    setLocalStorage("voluntarios", voluntarios);
+
+
+    console.log("array"+voluntarios);
+    mainVol.innerHTML = "";
+    voluntarios.map((voluntario)=> renderCard(voluntario,mainEl));
+
+    //para que se reseteen los campos del formulario
+    formEl.reset();
+
+
+});
